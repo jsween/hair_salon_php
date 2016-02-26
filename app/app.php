075a5985700c06815e0fe283b073e6cd0c835df6
Jon <jonsweeney26@gmail.com>
2016-02-26 11:25:04 -0800

    create a head

--- app/app.php
+++ app/app.php
@@ -3,16 +3,28 @@
     require_once __DIR__."/../src/Stylist.php";
     require_once __DIR__."/../src/Client.php";
 
-    $app = new Silex\Application();
+    // use Symfony\Component\Debug\Debug;
+    // Debug::enable();
 
     $server = 'mysql:host=localhost;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
+    $app = new Silex\Application();
+    // $app['debug']=true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    use Symfony\Component\HttpFoundation\Request;
+	Request::enableHttpMethodParameterOverride();
+    /* Homepage -- shows stylists */
+    $app->get("/", function() use ($app)
+	{
+		return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll(), 'form' => false));
+	});
 
     return $app;
  ?>
